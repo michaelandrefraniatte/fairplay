aa93f5540a994f3296d4c57717efc37d42ece130
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-14 03:47:24 +0100

    Add about

--- Src/FairPlay/V1/FairPlay/FairPlay/Program.cs
+++ Src/FairPlay/V1/FairPlay/FairPlay/Program.cs
@@ -89,6 +89,15 @@ namespace FairPlay
         const uint MEM_RESERVE = 0x00002000;
         const uint PAGE_READONLY = 0x02;
         const uint PAGE_GUARD = 0x100;
+        private static void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
         static void Main()
         {
             MinimizeConsoleWindow();
--- Src/FairPlay/V2/FairPlay/FairPlay/Program.cs
+++ Src/FairPlay/V2/FairPlay/FairPlay/Program.cs
@@ -31,6 +31,15 @@ namespace FairPlay
         public static Thread thread;
         public static uint CurrentResolution = 0;
         private static bool closed = false;
+        private static void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
         static void Main()
         {
             MinimizeConsoleWindow();
